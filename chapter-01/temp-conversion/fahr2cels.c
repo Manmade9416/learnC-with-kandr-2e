1af6cc5aab602341c6a5033379e9ec8d23525790
Manmade <267263224+Manmade9416@users.noreply.github.com>
2026-07-26 14:20:05 +0200

    Temperature conversion table in chapter one

---
+++ chapter-01/temp-conversion/fahr2cels.c
@@ -0,0 +1,39 @@
+/* This program prints a conversion table of Fahrenheit
+ * temperatures to their Celsius equivalent using the formula:
+ * Celsius = (5/9)*(Fahrenheit - 32). It teaches the basic 
+ * syntax of arithmetics and how variables work in C. I have
+ * added a little complication by writing a separate function
+ * that just does the conversion instead of doing it all in
+ * main, I don't think either is better than the other.*/
+
+#include <stdio.h> // input output header from stdlib
+
+float fah_cel_converter(float fahr) { 
+    // takes a float arg and returns a float.
+    // Converts fahrenheit temperature to a celsius temperature
+    float celsius = (5.0 / 9.0) * (fahr - 32);
+    return celsius;
+}
+
+int main(void) {
+    // returns an integer as a success code &
+    // prints the fahrenheit to celsius conversion table
+    // using fah_cel_converter to do the conversion math.
+
+    // Print fancy heading for the table
+    printf("FAHRENHEIT TO CELSIUS\n");
+    printf("**********************\n");
+    printf("----------------------\n");
+
+    // for loop variant of the temp converter is much less
+    // verbose and doesn't require that I name a bunch of variables
+    // but maybe at the cost readability idk.
+    for (int fahr = 0; fahr <= 300; fahr += 20) {
+        printf("| %d\t   | %.2f\n", fahr, fah_cel_converter(fahr));
+    }
+    printf("----------------------\n");
+
+    return 0;
+
+}
+
