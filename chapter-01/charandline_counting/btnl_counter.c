c29e1f0239881105323eefe17fd5432301bdafa3
Manmade <267263224+Manmade9416@users.noreply.github.com>
2026-07-28 09:53:19 +0200

    blanks, tabs, and newlines counter

---
+++ chapter-01/charandline_counting/btnl_counter.c
@@ -0,0 +1,22 @@
+/* Blanks, Tabs and Newline Counter:
+ * From C reference: Only Space and Tabs are classified as
+ * Blanks. So we really counting Space, Tabs and Newlines
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    size_t count = 0.0;
+    int cha;
+
+    while ((cha = getchar()) != EOF) {
+
+        // Use '||' logical OR operator
+        if (cha == ' ' || cha == '\t' || cha == '\n') {
+            ++count;
+        }
+    }
+
+    printf("Tab, Space & Newline count = %zu", count);
+    return 0;
+}
