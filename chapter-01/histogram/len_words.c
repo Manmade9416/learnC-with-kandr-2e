ef109b27e11ee78b181d693c2f1abb83e828c5df
Manmade <267263224+Manmade9416@users.noreply.github.com>
2026-07-31 10:06:28 +0200

    Massive improve to histogram look and added handling for long words

--- chapter-01/histogram/len_words.c
+++ chapter-01/histogram/len_words.c
@@ -10,16 +10,19 @@
 #define GREEN   "\x1b[32m"
 #define RESET   "\x1b[0m"
 
+
+
 int main(void) {
     int cha;    // Hold individual chars
     int l = 0;  // how long the word is
-    int nword = 0;
-    char *word = malloc(100*sizeof(char)); // Hold word
-    memset(word, 0, 100);   // Wipe the entire array?
+
+    char *word = malloc(1000*sizeof(char)); // Hold word
+    memset(word, 0, 1000);   // Wipe the entire array?
+
     bool in_word = false;   // In word or not
     bool first = false;     // Track first word for title
 
-    while ((cha = getchar()) != EOF && l < 100*sizeof(char)) {
+    while ((cha = getchar()) != EOF && l < 1000*sizeof(char)) {
 
         if (cha != ' ' && cha != '\n' && cha != '\t') {
             word[l] = (char)cha;
@@ -36,21 +39,52 @@ int main(void) {
             if (!first) {
                 printf("\nWord Length Histogram\n");
                 printf("━━━━━━━━━━━━━━━━━━━━━\n");
-                printf("\nX\n");
+                printf("\n%-6sX\n", "");
                 first = true;
             }
             
-            printf("%d ", nword);
-            for (int i = 0; i <= l; ++i) 
-                printf(GREEN"█"RESET);
-            printf("\n");
+            if (l > 6) {
+                for (int i = 0; i < 3; ++i)
+                    putchar(word[i]);
+                printf("...┃");
+            }
+            else
+                printf("%-6s┃", word);
 
-            l = 0;  // Reset word len 
-            memset(word, 0, 100);
-            in_word = false;
-            ++nword;
+            if (l > 50) {
+                for (int i = 0; i < 50; ++i) {
+                    printf(GREEN"█"RESET); 
+                }
+                printf(">50");
+                printf("\n");
+            }
+            else {
+                for (int i = 0; i < l; ++i) 
+                    printf(GREEN"█"RESET);
+                printf("\n");
+            }
+
+            l = 0;
+            memset(word, 0, 1000);
+            in_word = false; 
+
+            if (cha == '\n')
+                break;
         }
     }
+    
+    // Print out the bottom piece of the histogram
+    printf("%-6s╋", "");
+
+    for (int i = 0; i < 53; ++i)
+        printf("━");
+
+    printf(" Y");
+    printf("\n");
+    printf("%-6s0", "");
+
+    for (int i = 10; i <= 49; i += 10)
+        printf("          %d", i);
 
     free(word);
     return 0;
