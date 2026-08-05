c7be3087e74577f63cd64777516e3a28e83daa1b
Manmade <267263224+Manmade9416@users.noreply.github.com>
2026-08-05 09:08:16 +0200

    Update Input reverse program: It now works on input line by line

--- chapter-01/char_arrays/reverse_input/reverse.c
+++ chapter-01/char_arrays/reverse_input/reverse.c
@@ -1,16 +1,45 @@
-/* Print out copy of input in reverse
+/* Copy input to output in reverse
  */
 
 #include <stdio.h>
 
-#define MAXLEN 100000
+#define MAXLEN 1024 // Max line length/size
+
+int getline2(char buff[]) {
+
+    int cha;
+    int count = 0;
+
+    while ((cha = getchar()) != EOF && count < (MAXLEN - 1)) {
+
+        if (cha == '\n') {
+            buff[count] = cha;
+            ++count;
+            break;
+        }
+
+        buff[count] = cha;
+        ++count;
+    }
+
+    buff[count] = '\0';
+    return count;
+}
+
 
 void reverser(char ori[],size_t len) {
+    /* Print's input string back to output in reverse
+     * 1 char at a time starting from the last char
+     * to the first in the string/char array
+     */
 
-    for (size_t i = len; i > 0; --i) {
+    for (size_t i = len - 1; i > 0; --i) {
+        if (ori[i] == '\n')
+            continue;
         putchar(ori[i]);
     }
     putchar(ori[0]);
+    putchar('\n');  // Advance to new line for readability
 }
 
 int main() {
@@ -20,11 +49,9 @@ int main() {
     char original[MAXLEN];
     memset(original, 0, MAXLEN);
 
-    while ((cha = getchar()) != EOF) {
-        original[len] = cha;
-        ++len;
+    while ((len = getline2(original)) > 0) {
+        reverser(original, len);
     }
 
-    reverser(original, len);
     return 0;
 }
