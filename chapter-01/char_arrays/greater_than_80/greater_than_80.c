de913af16efa9b21721b906dba9894d04b944d6b
Manmade <267263224+Manmade9416@users.noreply.github.com>
2026-08-06 08:44:57 +0200

    Refactor Greater Than 80 Char program: It now works line by line much easier to understand

--- chapter-01/char_arrays/greater_than_80/greater_than_80.c
+++ chapter-01/char_arrays/greater_than_80/greater_than_80.c
@@ -3,62 +3,43 @@
 
 #include <stdio.h>
 
-#define SHORTEST 80
-#define LONGEST  10000
-#define MAXLINES 100000
+#define MINLEN  80
+#define MAXLEN  1024
 
-void cpy(char from[], char to[], size_t start, size_t stop) {
-    /* Copy lines of 80 chars or more to the another
-     * array */
+int getline2(char buff[]) {
 
-    for (size_t i = start; i <= stop; ++i) {
-        if (from[i])
-            to[i] = from[i];
+    int cha;
+    int count = 0;
+
+    while ((cha = getchar()) != EOF && count < (MAXLEN - 1)) {
+
+        if (cha == '\n') {
+            buff[count] = cha;
+            ++count;
+            break;
+        }
+
+        buff[count] = cha;
+        ++count;
     }
+
+    buff[count] = '\0';
+    return count;
 }
 
 int main() {
 
-    int cha;
-    char line[LONGEST];
-    char long_lines[MAXLINES];
+    char line[MAXLEN];
+    int len;
+    memset(line, 0, MAXLEN);
 
-    memset(line, 0, LONGEST);
-    memset(long_lines, 0, MAXLINES);
-
-    size_t line_len = 0;
-    size_t start = 0;
-
-    while((cha = getchar()) != EOF) {
-        if (line_len < (LONGEST - 1) && cha != '\n') {
-            line[line_len] = cha;
-            ++line_len;
-        }
-        else if (line_len > (LONGEST -1))
-            continue;
-        else if (cha == '\n' && line_len < SHORTEST)
-            line_len = 0;
-
-        if (cha == '\n' && line_len >= SHORTEST) {
-            line[line_len] = cha;
-            ++line_len;
-            cpy(line, long_lines, start, line_len);
-            start = line_len;
+    while ((len = getline2(line)) > 0) {
+        
+        if (len >= MINLEN) {
+            printf("%s", line);
         }
 
-        // memset(line, 0, LONGEST);
+        memset(line, 0, MAXLEN);
     }
-
-    if (line_len)
-        // printf("%s", long_lines);
-        for (int i = 0; i <= line_len; ++i) {
-            if (long_lines[i] == '\n') {
-                putchar(long_lines[i]);
-                putchar('\n');
-            }
-            else 
-                putchar(long_lines[i]);
-        }
-
     return 0;
 }
