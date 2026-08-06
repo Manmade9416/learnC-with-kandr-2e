9e2fda53aaff5c6a1efdf03958e1c4cfcd65b532
Manmade <267263224+Manmade9416@users.noreply.github.com>
2026-08-06 11:11:35 +0200

    refactor detab program, I previously misunderstood the task but it does the same thing differently now

--- chapter-01/detab/detab.c
+++ chapter-01/detab/detab.c
@@ -1,22 +1,46 @@
-/* Replaces TABS with 4 spaces
- */
+/* 
+ * Replaces TABS the appropriate n of spaces.
+ * With n = 4, count every char and reset count to 0 when
+ * count >= n. Then when the char == \t, just do n - count
+ * to get the number of spaces to enter.
+ * finally \n char resets the count because the previous
+ * line's count is no longer needed 
+ * */
 
 #include <stdio.h>
 
-#define DETAB   4
+#define TABSTOP   4     /* This is n*/
 
 int main() {
 
     int cha;
+    int col = 0;    /* This is count*/
 
     while ((cha = getchar()) != EOF) {
 
-        if (cha == '\t') {
-            for (int i = 0; i <= DETAB; ++i)
-                putchar(' ');
-        }
-        else if (cha != '\t')
+        if (cha == '\n') {  /* reset count on \n*/
             putchar(cha);
+            col = 0;
+            continue;
+        }
+
+        if (cha == '\t') {
+            int i;
+            if (col != TABSTOP) {
+                for (i = 1; i <= (TABSTOP - col); ++i)
+                    putchar(' ');
+            }
+            else
+                for (i = 1; i <= TABSTOP; ++i)
+                    putchar(' ');
+            col = 0;
+        }
+        else if (cha != '\t') {
+            putchar(cha);
+            if (col >= TABSTOP)
+                col = 0;
+            ++col;
+        }
     }
 
     return 0;
