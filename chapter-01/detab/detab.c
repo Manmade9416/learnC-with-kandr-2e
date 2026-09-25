80da5fa5eea50e43f4998eba881d786e7480c58d
Manmade <267263224+Manmade9416@users.noreply.github.com>
2026-09-25 10:39:27 +0200

    refactor detab program

--- chapter-01/detab/detab.c
+++ chapter-01/detab/detab.c
@@ -1,47 +1,43 @@
-/* 
- * Replaces TABS the appropriate n of spaces.
- * With n = 4, count every char and reset count to 0 when
- * count >= n. Then when the char == \t, just do n - count
- * to get the number of spaces to enter.
- * finally \n char resets the count because the previous
- * line's count is no longer needed 
- * */
-
+/* k&r excercise 1-20
+ * detab program
+ */
 #include <stdio.h>
 
-#define TABSTOP   4     /* This is n*/
+#define TAB_STOP    8  /* Tab stop ever 8 columns */
+#define TAB         '\t'  /* Char we're looking for*/
+#define SPACE       ' ' /* char to replace tabs by*/
+#define NEW_LINE    '\n'
 
-int main() {
+int main()
+{
+    int c;
+    size_t w = 0;      /* width/column num */
+    size_t nsp = 0;     /* number of spaces to insert*/
 
-    int cha;
-    int col = 0;    /* This is count*/
+    while ((c = getchar()) != EOF)
+    {
+        if (c == TAB)
+        {
+            /* will always result in a result > 0*/
+            nsp = TAB_STOP - (w % TAB_STOP);
+            
+            /* increment w by n spaces, we'll be adding*/
+            w += nsp;  
 
-    while ((cha = getchar()) != EOF) {
-
-        if (cha == '\n') {  /* reset count on \n*/
-            putchar(cha);
-            col = 0;
-            continue;
-        }
-
-        if (cha == '\t') {
-            int i;
-            if (col != TABSTOP) {
-                for (i = 1; i <= (TABSTOP - col); ++i)
-                    putchar(' ');
+            while (nsp > 0)
+            {
+                putchar(SPACE);
+                --nsp;
             }
-            else
-                for (i = 1; i <= TABSTOP; ++i)
-                    putchar(' ');
-            col = 0;
+
         }
-        else if (cha != '\t') {
-            putchar(cha);
-            if (col >= TABSTOP)
-                col = 0;
-            ++col;
+        else
+        {
+            putchar(c);
+
+            /* if at newline then must reset w to 0*/
+            (c == NEW_LINE) ? w = 0 : ++w;
         }
     }
-
     return 0;
 }
